158c6035253dd72113754bea1281436435cd7673
Wu, Chunyuan <chunyuan.wu@intel.com>
2025-03-12 13:22:48 +0000

    add wrapper for attention OPs

--- sgl-kernel/src/sgl-kernel/cpu.py
+++ sgl-kernel/src/sgl-kernel/cpu.py
@@ -24,3 +24,63 @@ def fused_experts(
 
 def convert_weight_packed(weight):
     return sgl_kernel.ops._kernels.convert_weight_packed(weight)
+
+
+def decode_attention(
+    q,
+    k_buffer,
+    v_buffer,
+    o,
+    kv_indptr,
+    kv_indices,
+    seq_lens,
+    attn_logits,
+    sm_scale,
+    logit_cap=0.0,
+):
+    sgl_kernel.ops._kernels.decode_attention_cpu(
+        q,
+        o,
+        k_buffer,
+        v_buffer,
+        attn_logits,
+        kv_indptr,
+        kv_indices,
+        seq_lens,
+        sm_scale,
+        logit_cap,
+    )
+
+
+def extend_attention(
+    q_extend,
+    k_extend,
+    v_extend,
+    o_extend,
+    k_buffer,
+    v_buffer,
+    req_to_tokens,
+    req_pool_indices,
+    seq_lens,
+    extend_seq_lens,
+    extend_start_loc,
+    max_len_extend,
+    sm_scale,
+    logit_cap=0.0,
+):
+    sgl_kernel.ops._kernels.extend_attention_cpu(
+        q_extend,
+        k_extend,
+        v_extend,
+        o_extend,
+        k_buffer,
+        v_buffer,
+        req_to_tokens,
+        req_pool_indices,
+        seq_lens,
+        extend_seq_lens,
+        extend_start_loc,
+        max_len_extend,
+        sm_scale,
+        logit_cap,
+    )
