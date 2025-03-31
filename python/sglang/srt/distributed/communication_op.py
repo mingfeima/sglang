d25837c2b323f3070cb0163c6339aca06464f082
Zheng, Beilei <beilei.zheng@intel.com>
2025-03-30 23:57:49 -0700

    optimize all_gather

--- python/sglang/srt/distributed/communication_op.py
+++ python/sglang/srt/distributed/communication_op.py
@@ -26,6 +26,13 @@ def tensor_model_parallel_all_gather(
     input_: torch.Tensor, dim: int = -1
 ) -> torch.Tensor:
     """All-gather the input tensor across model parallel group."""
+    if input_.is_cpu:
+        from sglang.srt.distributed import get_tp_group
+
+        shm_comm_op = get_tp_group().shm_comm_op
+        return shm_comm_op.shm_allgather(
+            input_, get_tp_group().device_group, dim
+        )
     return get_tp_group().all_gather(input_, dim)
 
 
