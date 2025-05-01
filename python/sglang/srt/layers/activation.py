0f9b286073301258c3a9e51810a3fcececfb8772
Thien Tran <gau.nernst@yahoo.com.sg>
2025-05-01 13:38:50 +0800

    remove cpu_has_amx_support() check at runtime

--- python/sglang/srt/layers/activation.py
+++ python/sglang/srt/layers/activation.py
@@ -55,11 +55,11 @@ class SiluAndMul(CustomOp):
         silu_and_mul(x, out)
         return out
 
-    def forward_cpu(self, x: torch.Tensor) -> torch.Tensor:
-        if cpu_has_amx_support():
-            return sgl_kernel.cpu.silu_and_mul(x)
-        else:
-            return self.forward_native(x)
+    forward_cpu = (
+        staticmethod(sgl_kernel.cpu.silu_and_mul)
+        if cpu_has_amx_support()
+        else forward_native
+    )
 
 
 class GeluAndMul(CustomOp):
